48696e12c2060fc9df151e182a0219d9e7c5c37a
anuragsahay5 <anuragtbg@gmail.com>
2023-02-13 23:54:03 +0530

    Single client Completed

---
+++ A3/Extension/client.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+short msgLen;
+char temp;
+char msgType;
+int msgSize;
+char msg[600];
+int socket_fd;
+pthread_t pid;
+
+void readResponse()
+{
+    int j;
+    while (1)
+    {
+        int rn = read(socket_fd, msg, 600);
+        if (!rn)
+        {
+            pthread_exit(NULL);
+            close(socket_fd);
+            printf("Server is Down\n");
+            exit(0);
+        }
+
+        printf("Recieved data : ");
+        j = 5;
+        if (msgType == '1')
+        {
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%c ", msg[j]);
+                j++;
+            }
+        }
+        else if (msgType == '2')
+        {
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%hi ", msg[j]);
+                j += 2;
+            }
+        }
+
+        else if (msgType == '4')
+        {
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%d ", msg[j]);
+                j += 4;
+            }
+        }
+        printf("\n\n\n");
+    }
+}
+
+int main()
+{
+    struct sockaddr_in server_addr;
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        perror("Socket creation failed");
+        exit(0);
+    }
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int status = connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (status < 0)
+    {
+        perror("Connection with server Failed");
+        exit(0);
+    }
+
+    printf("Connected to Server Successfully\n\n");
+
+    pthread_create(&pid, NULL, readResponse, NULL);
+
+    printf("Message Types\n");
+    printf("char : 1\n");
+    printf("short : 2\n");
+    printf("int : 4\n\n");
+
+    while (1)
+    {
+
+        /* Sending Response to Server*/
+        printf("Length : ");
+        scanf("%hi", &msgLen);
+        if (msgLen == 0)
+        {
+            pthread_cancel(pid);
+            break;
+        }
+        printf("Type : ");
+        scanf(" %c", &msgType);
+
+        if (msgType == '1')
+        {
+            msgSize = sizeof(char);
+        }
+        else if (msgType == '2')
+        {
+            msgSize = sizeof(short);
+        }
+        else if (msgType == '4')
+        {
+            msgSize = sizeof(int);
+        }
+
+        bzero(msg, 600);
+        msg[0] = '0';
+        memcpy(&msg[1], &msgLen, sizeof(short));
+        msg[3] = '1';
+        msg[4] = msgType;
+
+        int j = 5;
+        printf("Data : ");
+        if (msgType == '1')
+        {
+            for (int i = 0; i < msgLen; i++)
+            {
+                scanf(" %c", &msg[j]);
+                j++;
+            }
+        }
+        else if (msgType == '2')
+        {
+            short inp;
+            for (int i = 0; i < msgLen; i++)
+            {
+                scanf("%hi", &inp);
+                memcpy(&msg[j], &inp, sizeof(short));
+                j += 2;
+            }
+        }
+        else if (msgType == '4')
+        {
+            int inp;
+            for (int i = 0; i < msgLen; i++)
+            {
+                scanf("%d", &inp);
+                memcpy(&msg[j], &inp, sizeof(int));
+                j += 4;
+            }
+        }
+
+        int sn = write(socket_fd, msg, 600);
+        if (sn)
+        {
+            printf("Request Sent\n");
+        }
+        sleep(1);
+        
+        /* Recieving Response from Server*/
+        // readResponse();
+    }
+
+    close(socket_fd);
+    printf("Disconnected Succesfully\n");
+    return 0;
+}
---
+++ A3/Extension/server.c
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+
+void sort(char *msg, short msgLen, char msgType)
+{
+    if (msgType == '1')
+    {
+        int j = 5;
+        char arr[msgLen];
+        for (int i = 0; i < msgLen; i++)
+        {
+            arr[i] = msg[j];
+            j++;
+        }
+
+        char key;
+        for (int i = 1; i < msgLen; i++)
+        {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        j = 5;
+        for (int i = 0; i < msgLen; i++)
+        {
+            msg[j] = arr[i];
+            j++;
+        }
+    }
+
+    else if (msgType == '2')
+    {
+        int j = 5;
+        short arr[msgLen];
+        for (int i = 0; i < msgLen; i++)
+        {
+            memcpy(&arr[i], &msg[j], sizeof(short));
+            j += 2;
+        }
+
+        short key;
+        for (int i = 1; i < msgLen; i++)
+        {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+
+        j = 5;
+        for (int i = 0; i < msgLen; i++)
+        {
+            memcpy(&msg[j], &arr[i], sizeof(short));
+            j += 2;
+        }
+    }
+
+    else if (msgType == '4')
+    {
+        int j = 5;
+        int arr[msgLen];
+        for (int i = 0; i < msgLen; i++)
+        {
+            memcpy(&arr[i], &msg[j], sizeof(int));
+            j += 4;
+        }
+
+        int key;
+        for (int i = 1; i < msgLen; i++)
+        {
+            key = arr[i];
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+
+        j = 5;
+        for (int i = 0; i < msgLen; i++)
+        {
+            memcpy(&msg[j], &arr[i], sizeof(int));
+            j += 4;
+        }
+    }
+}
+
+int main()
+{
+    int socket_fd, socket_client;
+    struct sockaddr_in server_addr, client_addr;
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0)
+    {
+        perror("Socket creation failed");
+        exit(0);
+    }
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int status = bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+    if (status < 0)
+    {
+        perror("Bind Failed");
+        exit(0);
+    }
+
+    status = listen(socket_fd, 1);
+    if (status < 0)
+    {
+        perror("Listening Failed");
+        exit(0);
+    }
+
+    int client_addr_length = sizeof(client_addr);
+
+    bzero(&client_addr, sizeof(client_addr));
+    socket_client = accept(socket_fd, (struct sockaddr *)&client_addr, &client_addr_length);
+    if (socket_client < 0)
+    {
+        perror("Cannot establish connection with client");
+        exit(0);
+    }
+
+    char msg[600];
+    while (1)
+    {
+        bzero(msg, 600);
+        int rn = read(socket_client, msg, 600);
+        if (rn)
+        {
+            printf("Request recieved\n");
+            printf("Recieved data : ");
+        }
+
+        short msgLen;
+        char msgType;
+
+        memcpy(&msgLen, &msg[1], sizeof(short));
+        msgType = msg[4];
+
+        if (msgType == '1')
+        {
+            int j = 5;
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%c ", msg[j]);
+                j++;
+            }
+            sort(&msg[0], msgLen, msgType);
+        }
+        else if (msgType == '2')
+        {
+            int j = 5;
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%hi ", msg[j]);
+                j += 2;
+            }
+            sort(&msg[0], msgLen, msgType);
+        }
+
+        else if (msgType == '4')
+        {
+            int j = 5;
+            for (int i = 0; i < msgLen; i++)
+            {
+                printf("%d ", msg[j]);
+                j += 4;
+            }
+            sort(&msg[0], msgLen, msgType);
+        }
+        int sn = write(socket_client, msg, 600);
+
+        if (sn)
+        {
+            printf("\nResponse sent Successfully\n\n");
+        }
+    }
+
+    close(socket_fd);
+    close(socket_client);
+    return 0;
+}
